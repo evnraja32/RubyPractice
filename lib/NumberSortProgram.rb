b9ffa044c1f0ce3fa061b1934660a38050ec07cb
evnraja32 <evnraja32@gmail.com>
2018-01-10 14:12:00 +0530

    na

---
+++ lib/NumberSortProgram.rb
@@ -0,0 +1,24 @@
+#========================================================================================
+# File: NumberSortProgram.rb
+#
+# DESCRIPTION
+#     This program will sort the numbers in a given array into ascending and descending
+# order according to user selection.
+#
+#========================================================================================
+
+
+class NumberSort
+  
+  def initialize(arrayToSort = {})
+    begin
+      unless arrayToSort.empty?
+        raise "Please input the number to start sorting"
+      end
+    rescue
+      
+    end
+  end
+  
+  
+end
